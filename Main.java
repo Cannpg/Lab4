3c428acd0224937b8431ea1ddb3aa8b1cee452fb
cannpg <80770043+Cannpg@users.noreply.github.com>
2021-09-17 01:55:25 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,37 @@
+/*
+Lab 4
+Partners: 
+Parker Cann
+Date: (whatever date you turn it in)
+*/
+
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("This is Lab 4");
+
+    Random rand = new Random();
+
+    int randomNum = rand.nextInt(51);
+    System.out.println("The random number is: " + randomNum);
+
+    int decNum = randomNum;
+
+    while(decNum != 0){
+      decNum--;
+      System.out.println(decNum);
+    }
+
+    if(randomNum <= 5){
+      System.out.println("Ahoy mateys!");
+    }
+    else if(randomNum > 25 && randomNum < 42){
+      System.out.println("Cannonball!");
+    }
+    else{
+      System.out.println("Blast off!");
+    }
+
+  }
+}
